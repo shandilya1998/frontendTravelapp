fd9afc78ec5e04a21e1a499fa64dabdfbf2e4ae1
shandilya1998 <shandilya.shreyas@gmail.com>
2020-01-04 18:44:08 +0530

    commit redux integration untested

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
+import React, {Component} from 'react';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { createAppContainer } from 'react-navigation'; 
 import TripsScreens from './containers/TripsScreens';
 import ProfileScreens from './containers/ProfileScreens';
 import PlacesDirectoryScreens from './containers/PlacesDirectoryScreens';
+import {Provider, connect} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import reducer from './components/reducer_v1';
+import actions from './components/actions_v1';
 
-const App = createBottomTabNavigator({
+const Tab = createBottomTabNavigator({
   Trips : TripsScreens,
   Places : PlacesDirectoryScreens,
   Profile : ProfileScreens
@@ -13,5 +18,25 @@ const App = createBottomTabNavigator({
   initialRouteParams : 'Places'
 }
 );
+
+const middlewares = [thunk];
+
+const store = createStore(reducer, applyMiddleWare(...middlewares));
+
+export default class App extends Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<Provider store = {store}>
+				<View>
+					<Tab/>
+				</View>
+			</Provider>
+		);
+	}
+}
 
 export default createAppContainer(App);
